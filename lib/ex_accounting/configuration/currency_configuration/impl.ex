c4fd9075f81e565bf4f1678d2ffe7eb140590dd8
skusunoki <skusunoki@gmail.com>
2024-09-02 07:29:43 +0900

    chore: update todo comment

--- lib/ex_accounting/configuration/currency_configuration/impl.ex
+++ lib/ex_accounting/configuration/currency_configuration/impl.ex
@@ -3,6 +3,7 @@ defmodule ExAccounting.Configuration.CurrencyConfiguration.Impl do
     [currency | currencies]
   end
 
+  # TODO: Implement the datba base read function
   def cent_factor(currency) do
     case currency do
       :USD -> 100
